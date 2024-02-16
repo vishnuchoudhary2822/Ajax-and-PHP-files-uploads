19eec3d60db52226368487b8c091b774d1b197e2
Vishnu Choudhary <vishnu@ITPTek.com>
2024-02-16 16:32:44 +0530

    first commit

---
+++ check-exists.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Copyright (c) 2023 Reactive Apps, Vishnu Choudhary
+*/
+
+// Define a destination
+$targetFolder = '/uploads'; // Relative to the root and should match the upload folder in the uploader script
+
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . $targetFolder . '/' . $_POST['filename'])) {
+	echo 1;
+} else {
+	echo 0;
+}
+?>
---
+++ uploadifive.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Copyright (c) 2023 Reactive Apps, Vishnu Choudhary
+*/
+
+// Set the uplaod directory
+$uploadDir = '/uploads/';
+
+// Set the allowed file extensions
+$fileTypes = array('jpg', 'jpeg', 'gif', 'png'); // Allowed file extensions
+
+$verifyToken = md5('unique_salt' . $_POST['timestamp']);
+
+if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
+	$tempFile   = $_FILES['Filedata']['tmp_name'];
+	$uploadDir  = $_SERVER['DOCUMENT_ROOT'] . $uploadDir;
+	$targetFile = $uploadDir . $_FILES['Filedata']['name'];
+
+	// Validate the filetype
+	$fileParts = pathinfo($_FILES['Filedata']['name']);
+	if (in_array(strtolower($fileParts['extension']), $fileTypes)) {
+
+		// Save the file
+		move_uploaded_file($tempFile, $targetFile);
+		echo 1;
+
+	} else {
+
+		// The file type wasn't allowed
+		echo 'Invalid file type.';
+
+	}
+}
+?>
